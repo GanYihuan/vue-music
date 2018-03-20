4ec04aa9159c845eb25aa447681e4abe0413f29c
GanYihuan <583520052@qq.com>
2018-03-20 22:41:08 +0800

    7-25 mixin.js: 公共代码 设置mini播放器的正确位置显示

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import {mapMutations} from 'vuex'
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
