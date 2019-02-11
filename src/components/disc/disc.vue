61b44367ff871149437ccf0e8ab2e332e4018d65
GanYihuan <583520052@qq.com>
2019-02-11 16:05:32 +0800

    chapter9

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -16,6 +16,7 @@ import { getSongList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 import { mapGetters } from 'vuex'
 import { createSong } from 'common/js/song'
+
 export default {
   components: {
     MusicList
